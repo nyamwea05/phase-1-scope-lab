138ac6e711205fec145bf8e5473ccd47ffe40473
sandra <nyamweasandra@gmail.com>
2023-03-18 02:09:36 +0300

    Completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your solution in this file!
+var customerName= 'bob'
+function upperCaseCustomerName(){
+return customerName= customerName .toUpperCase();
+
+}
+
+const leastFavoriteCustomer='michelle'
+function setBestCustomer(){
+    bestCustomer= 'not bob'
+}
+function overwriteBestCustomer(){
+    bestCustomer= 'maybe bob'
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer='Michael'
+}
